12845e748c54bf55f956ad7493aee62ff60dd6ab
berak <px1704@web.de>
2018-02-24 09:23:26 +0100

    cmd: run gofmt -s -w

--- cmd/caffe-classifier/main.go
+++ cmd/caffe-classifier/main.go
@@ -21,113 +21,113 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "image"
-    "image/color"
-    "os"
-    "strconv"
+	"bufio"
+	"fmt"
+	"image"
+	"image/color"
+	"os"
+	"strconv"
 
-    "gocv.io/x/gocv"
+	"gocv.io/x/gocv"
 )
 
 // readDescriptions reads the descriptions from a file
 // and returns a slice of its lines.
 func readDescriptions(path string) ([]string, error) {
-    file, err := os.Open(path)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-    return lines, scanner.Err()
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
 
 func main() {
-    if len(os.Args) < 5 {
-        fmt.Println("How to run:\ncaffe-classifier [camera ID] [protofile] [modelfile] [descriptionsfile]")
-        return
-    }
+	if len(os.Args) < 5 {
+		fmt.Println("How to run:\ncaffe-classifier [camera ID] [protofile] [modelfile] [descriptionsfile]")
+		return
+	}
 
-    // parse args
-    deviceID, _ := strconv.Atoi(os.Args[1])
-    proto := os.Args[2]
-    model := os.Args[3]
-    desc  := os.Args[4]
+	// parse args
+	deviceID, _ := strconv.Atoi(os.Args[1])
+	proto := os.Args[2]
+	model := os.Args[3]
+	desc := os.Args[4]
 
-    //  read the descriptions file (the classnames)
-    descriptions, err := readDescriptions(desc)
-    if err != nil {
-        fmt.Printf("Error reading the descriptions file : %v\n", desc)
-        return
-    }
+	//  read the descriptions file (the classnames)
+	descriptions, err := readDescriptions(desc)
+	if err != nil {
+		fmt.Printf("Error reading the descriptions file : %v\n", desc)
+		return
+	}
 
-    // read DNN classifier
-    net := gocv.ReadNetFromCaffe(proto, model)
-    if net.Empty() {
-        fmt.Printf("Error loading network from : %v %v\n", proto, model)
-        return
-    }
-    defer net.Close()
+	// read DNN classifier
+	net := gocv.ReadNetFromCaffe(proto, model)
+	if net.Empty() {
+		fmt.Printf("Error loading network from : %v %v\n", proto, model)
+		return
+	}
+	defer net.Close()
 
-    // open capture device
-    webcam, err := gocv.VideoCaptureDevice(int(deviceID))
-    if err != nil {
-        fmt.Printf("Error opening video capture device: %v\n", deviceID)
-        return
-    }
-    defer webcam.Close()
+	// open capture device
+	webcam, err := gocv.VideoCaptureDevice(int(deviceID))
+	if err != nil {
+		fmt.Printf("Error opening video capture device: %v\n", deviceID)
+		return
+	}
+	defer webcam.Close()
 
-    window := gocv.NewWindow("Caffe Classifier")
-    defer window.Close()
+	window := gocv.NewWindow("Caffe Classifier")
+	defer window.Close()
 
-    img := gocv.NewMat()
-    defer img.Close()
+	img := gocv.NewMat()
+	defer img.Close()
 
-    status := "Ready"
-    statusColor := color.RGBA{0, 255, 0, 0}
-    fmt.Printf("Start reading camera device: %v\n", deviceID)
+	status := "Ready"
+	statusColor := color.RGBA{0, 255, 0, 0}
+	fmt.Printf("Start reading camera device: %v\n", deviceID)
 
-    for {
-        if ok := webcam.Read(img); !ok {
-            fmt.Printf("Error cannot read device %d\n", deviceID)
-            return
-        }
-        if img.Empty() {
-            continue
-        }
+	for {
+		if ok := webcam.Read(img); !ok {
+			fmt.Printf("Error cannot read device %d\n", deviceID)
+			return
+		}
+		if img.Empty() {
+			continue
+		}
 
-        // convert image Mat to 224x224 blob that the classifier can analyze
-        blob := gocv.BlobFromImage(img, 1.0, image.Pt(224, 224), gocv.NewScalar(104, 117, 123, 0), false, false)
+		// convert image Mat to 224x224 blob that the classifier can analyze
+		blob := gocv.BlobFromImage(img, 1.0, image.Pt(224, 224), gocv.NewScalar(104, 117, 123, 0), false, false)
 
-        // feed the blob into the classifier
-        net.SetInput(blob, "data")
+		// feed the blob into the classifier
+		net.SetInput(blob, "data")
 
-        // run a forward pass thru the network
-        prob := net.Forward("prob")
+		// run a forward pass thru the network
+		prob := net.Forward("prob")
 
-        // reshape the results into a 1x1000 matrix
-        probMat := prob.Reshape(1, 1)
+		// reshape the results into a 1x1000 matrix
+		probMat := prob.Reshape(1, 1)
 
-        // determine the most probable classification
-        _, maxVal, _, maxLoc := gocv.MinMaxLoc(probMat)
+		// determine the most probable classification
+		_, maxVal, _, maxLoc := gocv.MinMaxLoc(probMat)
 
-        // display classification
-        status = fmt.Sprintf("description: %v, maxVal: %v\n", descriptions[maxLoc.X], maxVal)
-        gocv.PutText(img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, statusColor, 2)
+		// display classification
+		status = fmt.Sprintf("description: %v, maxVal: %v\n", descriptions[maxLoc.X], maxVal)
+		gocv.PutText(img, status, image.Pt(10, 20), gocv.FontHersheyPlain, 1.2, statusColor, 2)
 
-        blob.Close()
-        prob.Close()
-        probMat.Close()
+		blob.Close()
+		prob.Close()
+		probMat.Close()
 
-        window.IMShow(img)
-        if window.WaitKey(1) >= 0 {
-            break
-        }
-    }
+		window.IMShow(img)
+		if window.WaitKey(1) >= 0 {
+			break
+		}
+	}
 }
